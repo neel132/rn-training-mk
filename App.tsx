fac7ff8238a250c269350b0b44583a46bdb75c16
Apple <neelgala55@gmail.com>
2023-08-02 09:41:13 +0530

    Example for drawer navigator

--- App.tsx
+++ App.tsx
@@ -5,23 +5,29 @@ import React from 'react';
 // } from 'react-native';
 // import Home from './src/screens/Home/Home.class';
 import Home from './src/screens/Home/Home.func';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import List from './src/screens/ListScreen/List';
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Drawer.Navigator>
+        <Drawer.Screen component={Home} name='Home' />
+        <Drawer.Screen component={List} name="List" />
+      </Drawer.Navigator>
+      {/* <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen component={Home} name='Home'
         options={{
           headerShown: false,
         }}
         />
         <Stack.Screen component={List} name='List' />
-      </Stack.Navigator>
+      </Stack.Navigator> */}
     </NavigationContainer>
   );
 }
