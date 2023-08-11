f066e249cb816ae37c27c636900a3700de243a7a
Apple <neelgala55@gmail.com>
2023-08-11 10:06:54 +0530

    Firebase notification example

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 // import {
 //   SafeAreaView,
 // } from 'react-native';
@@ -14,11 +14,30 @@ import { Provider } from 'react-redux';
 import { persistor, store } from './src/redux/store';
 import Counter from './src/screens/Counter/Counter';
 import { PersistGate } from 'redux-persist/integration/react';
+import messaging from '@react-native-firebase/messaging';
 
 // const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
 function App() {
+  useEffect(() => {
+    messaging().getToken().then(token => {
+      // here we get unique device token, which helps us to fire notifications to targetted audience
+      console.log("Device token: ", token);
+    });
+    // For background notifications
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      // whenever user receives the notification in background, this method get triggered.
+      console.log('Message handled in the background!', remoteMessage);
+    });
+    // For foreground notifications
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      // whenever user receives the notification in foreground, this method get triggered.
+      console.log('Message handled in the foreground!', remoteMessage);
+    });
+
+    return unsubscribe;
+  }, [])
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
--- src/screens/Counter/Counter.js
+++ src/screens/Counter/Counter.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CounterButton from "../../components/CounterButton/CounterButton";
 import { useDispatch, useSelector } from "react-redux";
 import { increment } from "../../redux/reducers/counterReducer";
-
+import {PermissionsAndroid} from 'react-native';
 const Counter = () => {
 	const {value} = useSelector(state => state.counterReducer);
 	const dispatch = useDispatch();
+	useEffect(() => {
+		PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+	}, [])
 	return (
 		<CounterButton
 			counterValue={value}
